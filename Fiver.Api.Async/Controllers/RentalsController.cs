de6390083a5dcbab3d6f624a524b2fcdda0455ae
Tahir Naushasd <t.naushad25@gmail.com>
2017-07-12 12:42:29 +0100

    refactorings

--- Fiver.Api.Async/Controllers/RentalsController.cs
+++ Fiver.Api.Async/Controllers/RentalsController.cs
@@ -12,42 +12,25 @@ namespace Fiver.Api.Async.Controllers
         [HttpPost("request-rental", Name = "RequestRental")]
         public IActionResult RequestRental([FromBody]RequestRentalInputModel inputModel)
         {
-            var queueNo = "q-2";
-            return AcceptedAtRoute("CheckStatus", new { queueNo = queueNo });
+            return AcceptedAtRoute("CheckStatus", new { queueNo = "q-2" });
         }
 
         [HttpGet("check-status/{queueNo}", Name = "CheckStatus")]
         public IActionResult CheckStatus(string queueNo)
         {
             if (queueNo == "q-2") // pending
-            {
-                return Ok(new CheckStatusOutputModel
-                {
-                    Status = "Pending"
-                });
-            }
+                return Ok(new CheckStatusOutputModel { Status = "Pending" });
             else // complete
-            {
-                var refNo = "r-1";
-                return SeeOther("GetRental", new { refNo = refNo });
-            }
+                return SeeOther("GetRental", new { refNo = "r-1" });
         }
 
         [HttpGet("get-rental/{refNo}", Name = "GetRental")]
         public IActionResult GetRental(string refNo)
         {
             if (refNo == "r-1")
-            {
-                return Ok(new GetRentalOutputModel
-                {
-                    DeliveryEstimate = DateTime.Now.AddDays(2),
-                    RentalCost = 5.99
-                });
-            }
+                return Ok(new GetRentalOutputModel { DeliveryEstimate = DateTime.Now.AddDays(2) });
             else
-            {
                 return NotFound();
-            }
         }
     }
 }
--- Fiver.Api.Async/Models/Rentals/GetRentalOutputModel.cs
+++ Fiver.Api.Async/Models/Rentals/GetRentalOutputModel.cs
@@ -5,6 +5,5 @@ namespace Fiver.Api.Async.Models.Rentals
     public class GetRentalOutputModel
     {
         public DateTime DeliveryEstimate { get; set; }
-        public double RentalCost { get; set; }
     }
 }
